e025f1c168c0d24c1a46303efa0a33fa4c90de37
LeahRoberts <lrobe09@gmail.com>
2014-06-17 10:05:24 +1000

    Update fastq_parser.py

--- IS_novel_locations/FASTQ_parse_approach/fastq_parser.py
+++ IS_novel_locations/FASTQ_parse_approach/fastq_parser.py
@@ -6,28 +6,26 @@
 import glob
 import os
 from Bio import SeqIO
+import sys
 
-# Make a list of the read names from a txt file:
+# Make a list of the desired read names from a txt file:
 
 with open('list.txt', 'r') as f:
         read_ids = [line.strip() for line in f]
-        print len(read_ids)
+#       print len(read_ids)
 #	print read_ids
 
 # Takes the fastq read ID and compares it to the read names in the read_ids list
 # If the read is found in the read_ids list, it is written out to a "new_reads.fastq" file
 
-	in_file = raw_input("fastq file: ")
-        read = SeqIO.parse(in_file, "fastq")
-        for fastq_rec in read:
+	in_file = sys.argv[1]
+        reads = SeqIO.parse(in_file, "fastq")
+        output = open("new_reads.fastq", "w")
+        for fastq_rec in reads:
 #		print fastq_rec.id
 		if fastq_rec.id in read_ids:
-			print fastq_rec.id + " exists!"
-			exit()
-			output = open("new_reads.fastq", "a")
+#			print fastq_rec.id + " exists!"
+#			exit()
 			SeqIO.write(fastq_rec, output, "fastq")
 		
-		#	if fastq_rec in read_ids:
-                #	print "found one read"
-                #	newreads.append(read)
-		#	SeqIO.write(fastq_rec, output_file, "fastq")
+	
