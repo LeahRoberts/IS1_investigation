e002753a87e49a44ce57d29b1d7ea62c8457e815
Leah Roberts <l.roberts3@uq.edu.au>
2015-08-11 14:32:48 +1000

    adding ipynb stuff

---
+++ Easyfig_IS_Sliding_Window_graph/Generate_list_of_distances/BUILD.sh
@@ -0,0 +1,5 @@
+ipython nbconvert Simulator.ipynb
+ipython nbconvert Association.ipynb
+mv Simulator.html Association.html html
+ipython nbconvert Association.ipynb --to python
+ipython nbconvert Simulator.ipynb --to python
